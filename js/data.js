e9dd49380bfc3610b5723cea15e61f7a3967863c
Vera Freiman <fre-vera@yandex.ru>
2024-07-24 16:27:59 +0300

    Added js code for the database

---
+++ js/data.js
@@ -0,0 +1,218 @@
+export const header = {
+  langs: [
+    {
+      key: 'ru',
+      text: 'RU',
+    },
+    {
+      key: 'en',
+      text: 'EN',
+    },
+  ],
+  navigation: [
+    {
+      key: '#downloand',
+      text:'Downloand',
+    },
+    {
+      key: '#warranty',
+      text:'Warranty',
+    },
+    {
+      key: '#care',
+      text:'Care',
+    },
+    {
+      key: '#cashback',
+      text:'Cashback',
+    },
+    {
+      key: '#clients',
+      text:'Clients',
+    },
+  ],
+};
+
+export const download = {
+  title: 'Title text',
+  texts: ['Some paragraph 1'],
+  linkApple: {
+    url: 'https://apple.com',
+    source: './assets/icons/apple.svg',
+  },
+  linkGoogle: {
+    url: 'https://google.com',
+    source: './assets/icons/google.svg',
+  },
+  image: {
+    source: './assets/images/1.jpg',
+    description: 'Bag and device',
+  },
+};
+
+export const warranty = {
+  title: 'Title text',
+  texts: ['Some paragraph 1', 'Some paragraph 2'],
+  image: {
+    source: './assets/images/2.jpg',
+    description: 'Basket',
+  },
+};
+
+export const care = {
+  title: 'Title text',
+  texts: ['Some paragraph 1', 'Some paragraph 2', 'Some paragraph 3'],
+  image: {
+    source: './assets/images/3.jpg',
+    description: 'Card',
+  },
+};
+
+export const cashback = {
+  title: 'Title text',
+  texts: ['Some paragraph 1', 'Some paragraph 2', 'Some paragraph 3'],
+  buttonText: 'Order a consultation',
+};
+
+export const clients = {
+  brands: [
+    {
+      lightSource: './assets/brands/light/1.svg',
+      darkSource: './assets/brands/dark/1.svg',
+      description: 'Logo client Rosegal',
+    },
+    {
+      lightSource: './assets/brands/light/2.svg',
+      darkSource: './assets/brands/dark/2.svg',
+      description: 'Logo client Gearbest',
+    },
+    {
+      lightSource: './assets/brands/light/3.svg',
+      darkSource: './assets/brands/dark/3.svg',
+      description: 'Logo client Allegro',
+    },
+    {
+      lightSource: './assets/brands/light/4.svg',
+      darkSource: './assets/brands/dark/4.svg',
+      description: 'Logo client Aliexpress',
+    },
+    {
+      lightSource: './assets/brands/light/5.svg',
+      darkSource: './assets/brands/dark/5.svg',
+      description: 'Logo client Booking.com',
+    },
+    {
+      lightSource: './assets/brands/light/6.svg',
+      darkSource: './assets/brands/dark/6.svg',
+      description: 'Logo client MediaMarkt',
+    },
+    {
+      lightSource: './assets/brands/light/7.svg',
+      darkSource: './assets/brands/dark/7.svg',
+      description: 'Logo client RTVeuroAGD',
+    },
+    {
+      lightSource: './assets/brands/light/8.svg',
+      darkSource: './assets/brands/dark/8.svg',
+      description: 'Logo client Groupon',
+    },
+  ]
+};
+
+export const footer = {
+  columns: [
+    {
+      title: 'Learn',
+      links: [
+        {
+          url: 'https://example.com',
+          text: 'Help center',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Join ZEN team',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Privacy Policy',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Cookies',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Terms of use',
+        },
+      ],
+    },
+    {
+      title: 'Discover',
+      links: [
+        {
+          url: 'https://example.com',
+          text: 'For developers',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Apple Pay',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Google Pay',
+        },
+      ],
+    },
+    {
+      title: 'Offer',
+      links: [
+        {
+          url: 'https://example.com',
+          text: 'Business',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Personal',
+        },
+      ],
+    },
+    {
+      title: 'Language',
+      links: [
+        {
+          url: 'https://example.com',
+          text: 'English',
+        },
+        {
+          url: 'https://example.com',
+          text: 'Polish',
+        },
+      ],
+    },
+    {
+      title: 'Follow us',
+      links: [
+        {
+          url: 'https://example.com',
+          source: './assets/icons/in.svg',
+          description: 'Linkedin',
+        },
+      ],
+    },
+    {
+      title: 'Contact',
+      links: [
+        {
+          email: 'hello@zen.com',
+        }
+      ],
+    },
+  ],
+  info: {
+    texts: ['Some paragraph 1', 'Some paragraph 2','Some paragraph 3', 'Some paragraph 4','Some paragraph 5', 'Some paragraph 6'],
+    developer: {
+      url: 'https://example.com',
+      source: './assets/icons/developer.svg',
+    },
+  },
+  };
