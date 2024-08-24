868a25cb412c1ec8d66a2d73cdf56908d949ba36
Vera Freiman <fre-vera@yandex.ru>
2024-08-24 16:02:58 +0300

    Fixed

--- js/widgets/Download/Download.js
+++ js/widgets/Download/Download.js
@@ -1,3 +1,6 @@
+import { Title } from '../../ui/Title/Title.js';
+import { LinkImages } from '../../ui/Links/Links.js';
+
 /**
  * @typedef {import('./types').DownloadData} DownloadData
 /**
@@ -8,30 +11,26 @@
  * @returns {string} HTML
  */
 
-import { Title } from '../../ui/Title/Title.js';
-import { LinkImages } from '../../ui/Links/Links.js';
-
-
 export const Download = (data) => {
   const { title, texts, links, image } = data;
   return `
-  <section class="download" id="download">
-    <div class="download__wrapper">
-      <div class="download__text-block">
-        ${Title(title, 'download__title')}
-        ${texts.map((text) => `<p class="download__copy">${text}</p>`).join('')}
-        ${links.map((link) => `
-          <div class="download__links">
-            <a class="download__link" href="${link.url}">
-              ${`<img src='${LinkImages[link.name]}' alt='${link.name}'>`}
-            </a>
-          </div>
-        `).join('')}
+    <section class="download" id="download">
+      <div class="download__wrapper">
+        <div class="download__text-block">
+          ${Title(title, 'download__title')}
+          ${texts.map((text) => `<p class="download__copy">${text}</p>`).join('')}
+          ${links.map((link) => `
+            <div class="download__links">
+              <a class="download__link" href="${link.url}">
+                ${`<img src='${LinkImages[link.name]}' alt='${link.name}'>`}
+              </a>
+            </div>
+          `).join('')}
+        </div>
+        <div class="download__image-block">
+          <img src="${image.source}" alt="${image.description}">
+        </div>
       </div>
-      <div class="download__image-block">
-        <img src="${image.source}" alt="${image.description}">
-      </div>
-    </div>
-  </section>
+    </section>
   `;
 };
