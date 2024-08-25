995ef58750f86e88f9c128f458dc1aa7baf517db
Vera Freiman <fre-vera@yandex.ru>
2024-08-25 16:38:11 +0300

    Added import for header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 import { data } from './data.js';
+import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
 
 const $root = document.querySelector('#root');
 
+$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
+
+const $logo = document.querySelector('#logo');
+
+$logo?.addEventListener('clik', () => {
+  console.log('clickedLogo');
+});
