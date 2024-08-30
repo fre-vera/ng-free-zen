96092f8e55eb380e7df9fcb01a2e514ae68a3044
Vera Freiman <fre-vera@yandex.ru>
2024-08-30 19:37:40 +0300

    Fix

--- js/widgets/Cashback/Cashback.js
+++ js/widgets/Cashback/Cashback.js
@@ -19,7 +19,7 @@ export const Cashback = (data) => {
         <div class="cashback__text-block">
           ${Title(title, 'cashback__title')}
           ${texts.map((text) => `<p class="cashback__copy">${text}</p>`).join('')}
-          ${CashbackButton(buttonText, 'cashback__button')}
+          ${CashbackButton('cashback__button', buttonText)}
         </div>
       </div>
     </section>
