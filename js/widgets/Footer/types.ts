d20afa2d5221f4f8cf77c2733a2248ceb2f7c666
Vera Freiman <fre-vera@yandex.ru>
2024-09-02 19:55:26 +0300

    Added component footer

---
+++ js/widgets/Footer/types.ts
@@ -0,0 +1,8 @@
+import type { CategorieData } from '../../ui/Column/types';
+import type { LinkData } from '../../ui/Column/types';
+
+export type FooterData = {
+  categories: CategorieData[];
+  texts: string[];
+  developer: LinkData;
+};
