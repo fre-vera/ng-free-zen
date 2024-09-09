bdcb41103e19293840e975eb7e5f4cd5e2ee428a
Vera Freiman <fre-vera@yandex.ru>
2024-09-09 20:12:51 +0300

    Added handleLogoClick

---
+++ js/handlers.js
@@ -0,0 +1,11 @@
+/**
+ * @function handleLogoClick
+ * @description Smooth scrolling up
+ */
+
+export const handleLogoClick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
