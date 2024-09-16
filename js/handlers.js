93d6e3a9f7e8afb5b351e87416a49f503af279b0
Vera Freiman <fre-vera@yandex.ru>
2024-09-16 14:07:54 +0300

    Added theme change function

--- js/handlers.js
+++ js/handlers.js
@@ -1,3 +1,6 @@
+import { IconMoon } from './ui/Icons/IconMoon/IconMoon.js';
+import { IconSun } from './ui/Icons/IconSun/IconSun.js';
+
 /**
  * @function handleLogoClick
  * @description Smooth scrolling up
@@ -7,5 +10,36 @@ export const handleLogoClick = () => {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
+  });
+};
+
+/**
+ * @function onThemeClick
+ * @description In an anonymous handler
+ * @param {Event} event
+ * @param {import('./widgets/Сlients/types.js').BrandFromAPI[]} brandsFromAPI
+ */
+
+export const onThemeClick = (event, brandsFromAPI) => {
+  const $root = document.querySelector('#root');
+  const $themeBtn = /** @type { HTMLElement | null } */ (event.currentTarget);
+  /** @type {NodeListOf<HTMLImageElement>} */
+  const $brandNodes = document.querySelectorAll('[data-id="brand"]');
+
+  if (!$root || !$themeBtn) return;
+
+  $root.classList.toggle('dark')
+    ? $root.classList.remove('light')
+    : $root.classList.add('light');
+
+  $themeBtn.dataset.theme = $themeBtn.dataset.theme === 'light' ? 'dark' : 'light';
+  $themeBtn.innerHTML = $themeBtn.dataset.theme === 'light'
+    ? IconMoon()
+    : IconSun();
+
+  $brandNodes.forEach((brand, index) => {
+    brand.src = $themeBtn.dataset.theme === 'light'
+      ? brandsFromAPI[index].logo.lightSource
+      : brandsFromAPI[index].logo.darkSource;
   });
 };
