595bd01cd6f0a1dbd0ac1e9ac6a0842c835c6419
Vera Freiman <fre-vera@yandex.ru>
2024-09-16 14:10:13 +0300

    Changed data and connected function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { data } from './data.js';
+import { data as dataFromAPI } from './data.js';
 import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
 import { Warranty } from './widgets/Warranty/Warranty.js';
@@ -6,18 +6,20 @@ import { Care } from './widgets/Care/Care.js';
 import { Cashback } from './widgets/Cashback/Cashback.js';
 import { Clients } from './widgets/Сlients/Clients.js';
 import { Footer } from './widgets/Footer/index.js';
-import { handleLogoClick } from './handlers.js';
+import { handleLogoClick, onThemeClick } from './handlers.js';
 
 const $root = document.querySelector('#root');
 
-$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
-$root?.insertAdjacentHTML('beforeend', Download(data.download));
-$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
-$root?.insertAdjacentHTML('beforeend', Care(data.care));
-$root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
-$root?.insertAdjacentHTML('beforeend', Clients(data.clients));
-$root?.insertAdjacentHTML('beforeend', Footer(data.secondaryInfo));
+$root?.insertAdjacentHTML('beforeend', Header(dataFromAPI.primaryInfo));
+$root?.insertAdjacentHTML('beforeend', Download(dataFromAPI.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(dataFromAPI.warranty));
+$root?.insertAdjacentHTML('beforeend', Care(dataFromAPI.care));
+$root?.insertAdjacentHTML('beforeend', Cashback(dataFromAPI.cashback));
+$root?.insertAdjacentHTML('beforeend', Clients(dataFromAPI.clients));
+$root?.insertAdjacentHTML('beforeend', Footer(dataFromAPI.secondaryInfo));
 
 const $logoBtn = document.querySelector('#logo');
+const $themeBtn = document.querySelector('#theme');
 
 $logoBtn?.addEventListener('click', handleLogoClick);
+$themeBtn?.addEventListener('click', (event) => onThemeClick(event, dataFromAPI.clients.brands));
