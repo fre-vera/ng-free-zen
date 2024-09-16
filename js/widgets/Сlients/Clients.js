4e4c6a87cc2e2040bf97884e6a699b6064d22972
Vera Freiman <fre-vera@yandex.ru>
2024-09-16 14:13:36 +0300

    Changed function

--- js/widgets/Сlients/Clients.js
+++ js/widgets/Сlients/Clients.js
@@ -10,17 +10,13 @@
 
 export const Clients = (data) => {
   const { brands } = data;
-  // Находим элемент с классом 'app'
-  const appElement = document.querySelector('.app');
-  // Проверка, есть ли у корневого элемента класс 'dark'
-  const isDarkMode = document.documentElement.classList.contains('dark');
   return `
     <section class="clients" id="clients">
       <div class="clients__wrapper">
         <ul class="clients__brands">
           ${brands.map((brand) => `
             <li class="clients__brand">
-              <img src='${isDarkMode ? brand.logo.darkSource : brand.logo.lightSource}' alt='${brand.name}'>
+              <img src="${brand.logo.lightSource}" alt="${brand.name}" data-id = "brand">
             </li>
           `).join('')}
         </ul>
